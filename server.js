84c4bcb87876a47359aa61cd59dc347810100c80
Swanand Pagnis <swanand.pagnis@gmail.com>
2023-08-12 19:13:57 +0530

    Use $PORT

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = 4000;
+const PORT = process.env.PORT;
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
