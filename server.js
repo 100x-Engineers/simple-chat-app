e1ed2b449d4500ca07a36ad14af116968c43aa2f
Siddhant Goswami <goswami.siddh@gmail.com>
2023-08-15 15:50:05 +0530

    Added default PORT 3000

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
